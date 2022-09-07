362b13418f9fc39905fba5037f5a4ad0f0d733d4
pjordaan <pjordaan@users.noreply.github.com>
2022-09-07 18:37:27 +0000

    Update file(s) "packages/html-builders" from "apie-lib/apie-lib-monorepo"

--- src/Interfaces/ComponentInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Apie\HtmlBuilders\Interfaces;
-
-interface ComponentInterface
-{
-    public function getComponent(string $key): ComponentInterface;
-
-    public function getAttribute(string $key): mixed;
-}
--- src/Interfaces/ComponentRendererInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Apie\HtmlBuilders\Interfaces;
-
-interface ComponentRendererInterface
-{
-    public function render(ComponentInterface $componentInterface): string;
-}
