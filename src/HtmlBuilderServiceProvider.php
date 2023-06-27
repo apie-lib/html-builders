8657156526bba170e21c4f897ddc2652b6bee1f0
pjordaan <pjordaan@users.noreply.github.com>
2023-06-27 14:29:50 +0000

    Update file(s) "packages/html-builders/." from "apie-lib/apie-lib-monorepo"

---
+++ src/HtmlBuilderServiceProvider.php
@@ -0,0 +1,76 @@
+<?php
+namespace Apie\HtmlBuilders;
+
+use Apie\ServiceProviderGenerator\UseGeneratedMethods;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * This file is generated with apie/service-provider-generator from file: html_builders.yaml
+ * @codecoverageIgnore
+ * @phpstan-ignore
+ */
+class HtmlBuilderServiceProvider extends ServiceProvider
+{
+    use UseGeneratedMethods;
+
+    public function register()
+    {
+        $this->app->singleton(
+            \Apie\HtmlBuilders\Factories\ComponentFactory::class,
+            function ($app) {
+                return new \Apie\HtmlBuilders\Factories\ComponentFactory(
+                    $app->make(\Apie\HtmlBuilders\Configuration\ApplicationConfiguration::class),
+                    $app->make(\Apie\Core\BoundedContext\BoundedContextHashmap::class),
+                    $app->make(\Apie\HtmlBuilders\Factories\FormComponentFactory::class)
+                );
+            }
+        );
+        $this->app->singleton(
+            \Apie\HtmlBuilders\Factories\FormComponentFactory::class,
+            function ($app) {
+                return \Apie\HtmlBuilders\Factories\FormComponentFactory::create(
+                    $this->getTaggedServicesIterator(\Apie\HtmlBuilders\Interfaces\FormComponentProviderInterface::class)
+                );
+                
+            }
+        );
+        $this->app->singleton(
+            \Apie\HtmlBuilders\Factories\Concrete\DropdownOptionsComponentProvider::class,
+            function ($app) {
+                return new \Apie\HtmlBuilders\Factories\Concrete\DropdownOptionsComponentProvider(
+                    $app->make(\Apie\HtmlBuilders\Configuration\ApplicationConfiguration::class)
+                );
+            }
+        );
+        $this->app->tag([\Apie\HtmlBuilders\Factories\Concrete\DropdownOptionsComponentProvider::class], \Apie\HtmlBuilders\Interfaces\FormComponentProviderInterface::class);
+        $this->app->singleton(
+            \Apie\HtmlBuilders\Configuration\ApplicationConfiguration::class,
+            function ($app) {
+                return new \Apie\HtmlBuilders\Configuration\ApplicationConfiguration(
+                    array (
+                  'base_url' => '%apie.cms.base_url%',
+                )
+                );
+            }
+        );
+        $this->app->singleton(
+            \Apie\HtmlBuilders\Assets\AssetManager::class,
+            function ($app) {
+                return \Apie\HtmlBuilders\Assets\AssetManager::create(
+                    $this->parseArgument('%apie.cms.asset_folders%')
+                );
+                
+            }
+        );
+        $this->app->singleton(
+            \Apie\HtmlBuilders\Interfaces\ComponentRendererInterface::class,
+            function ($app) {
+                return \Apie\Common\Wrappers\CmsRendererFactory::createRenderer(
+                    $app->make(\Apie\HtmlBuilders\Assets\AssetManager::class)
+                );
+                
+            }
+        );
+        
+    }
+}
