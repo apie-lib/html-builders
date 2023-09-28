c391854779c8080554d9ea9ce2891de83e053629
pjordaan <pjordaan@users.noreply.github.com>
2023-09-28 07:37:17 +0000

    Update file(s) "packages/html-builders/." from "apie-lib/apie-lib-monorepo"

--- src/ErrorHandler/StacktraceRenderer.php
+++ src/ErrorHandler/StacktraceRenderer.php
@@ -18,6 +18,14 @@ final class StacktraceRenderer implements Stringable
         Ignition::make()->renderException($this->error);
         $html = ob_get_clean();
         $html = str_replace(['<html ', '</html>'], ['<div ', '</div>'], $html);
+        $html = str_replace('height:auto', '', $html);
+        $html .= '<script>(function () {
+        for (const elm of document.querySelectorAll("#app > nav")) { elm.remove(); }
+        const sections = document.querySelectorAll("main section");
+        // remove flare add:
+        sections[sections.length - 1].remove();
+        }());
+        </script>';
         return $html;
     }
 }
