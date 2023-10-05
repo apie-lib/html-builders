8e47318511599e49ee46374de3f424c4a94543a7
pjordaan <pjordaan@users.noreply.github.com>
2023-10-05 09:37:34 +0000

    Update file(s) "packages/html-builders/." from "apie-lib/apie-lib-monorepo"

--- src/Factories/Concrete/VerifyOtpInputComponentProvider.php
+++ src/Factories/Concrete/VerifyOtpInputComponentProvider.php
@@ -1,6 +1,7 @@
 <?php
 namespace Apie\HtmlBuilders\Factories\Concrete;
 
+use Apie\Common\ContextConstants;
 use Apie\Core\Utils\ConverterUtils;
 use Apie\HtmlBuilders\Components\Forms\VerifyOtpInput;
 use Apie\HtmlBuilders\FormBuildContext;
@@ -14,7 +15,7 @@ class VerifyOtpInputComponentProvider implements FormComponentProviderInterface
 {
     public function supports(ReflectionType $type, FormBuildContext $context): bool
     {
-        if (!$context->getApieContext()->hasContext('resource')) {
+        if (!$context->getApieContext()->hasContext(ContextConstants::RESOURCE)) {
             return false;
         }
         $class = ConverterUtils::toReflectionClass($type);
@@ -35,7 +36,7 @@ class VerifyOtpInputComponentProvider implements FormComponentProviderInterface
         $property = $class->getMethod('getOtpReference')->invoke(null);
         /** @var string $label */
         $label = $class->getMethod('getOtpLabel')->invoke(null);
-        $resource = $context->getApieContext()->getContext('resource');
+        $resource = $context->getApieContext()->getContext(ContextConstants::RESOURCE);
         $otpSecret = $property->getValue($resource);
 
         return new VerifyOtpInput(
