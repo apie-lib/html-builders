d030a15144544a6520cdd84c1b29f4485680966b
pjordaan <pjordaan@users.noreply.github.com>
2023-10-19 15:16:30 +0000

    Update file(s) "packages/html-builders/." from "apie-lib/apie-lib-monorepo"

--- src/FieldDisplayProviders/ValueObjectDisplayProvider.php
+++ src/FieldDisplayProviders/ValueObjectDisplayProvider.php
@@ -1,16 +1,18 @@
 <?php
 namespace Apie\HtmlBuilders\FieldDisplayProviders;
 
+use Apie\Core\Utils\ValueObjectUtils;
 use Apie\Core\ValueObjects\Interfaces\ValueObjectInterface;
 use Apie\HtmlBuilders\FieldDisplayBuildContext;
 use Apie\HtmlBuilders\Interfaces\ComponentInterface;
 use Apie\HtmlBuilders\Interfaces\FieldDisplayComponentProviderInterface;
+use ReflectionClass;
 
 final class ValueObjectDisplayProvider implements FieldDisplayComponentProviderInterface
 {
     public function supports(mixed $object, FieldDisplayBuildContext $context): bool
     {
-        return $object instanceof ValueObjectInterface;
+        return $object instanceof ValueObjectInterface && ValueObjectUtils::isNonCompositeValueObject(new ReflectionClass($object));
     }
     public function createComponentFor(mixed $object, FieldDisplayBuildContext $context): ComponentInterface
     {
