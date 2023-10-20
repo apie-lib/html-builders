91b9fe95e030861a9e1168e93c83932b4ce3d999
pjordaan <pjordaan@users.noreply.github.com>
2023-10-20 07:50:25 +0000

    Update file(s) "packages/html-builders/." from "apie-lib/apie-lib-monorepo"

--- src/Lists/ActionList.php
+++ src/Lists/ActionList.php
@@ -3,10 +3,11 @@ namespace Apie\HtmlBuilders\Lists;
 
 use Apie\Core\Lists\ItemList;
 use Apie\HtmlBuilders\ResourceActions\ResourceActionInterface;
+use Apie\HtmlBuilders\ResourceActions\SingleResourceActionInterface;
 
 class ActionList extends ItemList
 {
-    public function offsetGet(mixed $offset): ResourceActionInterface
+    public function offsetGet(mixed $offset): ResourceActionInterface|SingleResourceActionInterface
     {
         return parent::offsetGet($offset);
     }
