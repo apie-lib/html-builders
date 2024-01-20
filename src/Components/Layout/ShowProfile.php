81c0f7c1bfbbfea71e99f775091c86047a31b4db
pjordaan <pjordaan@users.noreply.github.com>
2024-01-20 22:05:27 +0000

    Update file(s) "packages/html-builders/." from "apie-lib/apie-lib-monorepo"

--- src/Components/Layout/ShowProfile.php
+++ src/Components/Layout/ShowProfile.php
@@ -2,30 +2,61 @@
 namespace Apie\HtmlBuilders\Components\Layout;
 
 use Apie\CommonValueObjects\Email;
+use Apie\Core\Context\ApieContext;
 use Apie\Core\Entities\EntityInterface;
 use Apie\Core\ValueObjects\Exceptions\InvalidStringForValueObjectException;
+use Apie\Core\ValueObjects\Utils;
 use Apie\HtmlBuilders\Components\BaseComponent;
 use Apie\HtmlBuilders\Configuration\CurrentConfiguration;
 use Apie\HtmlBuilders\Lists\ComponentHashmap;
+use Apie\Serializer\Serializer;
 use ReflectionClass;
 use Symfony\Component\PropertyAccess\PropertyAccessorBuilder;
 
 class ShowProfile extends BaseComponent
 {
+    private const PROFILE_FIELDS = [
+        ['email', 'username', 'id'],
+        ['fullName', 'name', 'firstName', 'lastName'],
+    ];
+
+    /**
+     * @return array<string, string>
+     */
+    private function buildFields(EntityInterface $user, ApieContext $apieContext): array
+    {
+        $fields = [];
+        if ($apieContext->hasContext(Serializer::class)) {
+            $serializer = $apieContext->getContext(Serializer::class);
+            assert($serializer instanceof Serializer);
+            $allFields = Utils::toArray($serializer->normalize($user, $apieContext));
+            foreach (self::PROFILE_FIELDS as $profileFieldList) {
+                foreach ($profileFieldList as $profileField) {
+                    if (isset($allFields[$profileField])) {
+                        $fields[$profileField] = $allFields[$profileField];
+                        break;
+                    }
+                }
+            }
+            return $fields;
+        }
+        $propertyAccess = (new PropertyAccessorBuilder())->getPropertyAccessor();
+        foreach (self::PROFILE_FIELDS as $profileFieldList) {
+            foreach ($profileFieldList as $profileField) {
+                if ($propertyAccess->isReadable($user, $profileField)) {
+                    $fields[$profileField] = $propertyAccess->getValue($user, $profileField);
+                    break;
+                }
+            }
+        }
+        return $fields;
+    }
+
     public function __construct(
         CurrentConfiguration $currentConfiguration,
         EntityInterface $user
     ) {
-        $propertyAccess = (new PropertyAccessorBuilder())->getPropertyAccessor();
-        $email = null;
-        if ($propertyAccess->isReadable($user, 'email')) {
-            $email = $propertyAccess->getValue($user, 'email');
-        } else if ($propertyAccess->isReadable($user, 'id')) {
-            try {
-                $email = (new Email($propertyAccess->getValue($user, 'id')))->toNative();
-            } catch (InvalidStringForValueObjectException) {
-            }
-        }
+        $fields = $this->buildFields($user, $currentConfiguration->getApieContext());
         $baseClass = $user->getId()::getReferenceFor();
         $boundedContextId = $currentConfiguration->getSelectedBoundedContextId();
         $boundedContextId = $currentConfiguration->getBoundedContextHashmap()->getBoundedContextFromClassName(
@@ -39,9 +70,11 @@ class ShowProfile extends BaseComponent
         parent::__construct(
             [
                 'user' => $user,
-                'email' => $email,
+                'email' => $fields['email'] ?? null,
                 'profileUrl' => $profileUrl,
-                'gravatarUrl' => 'https://gravatar.com/avatar/' . md5(strtolower($email ?? '')),
+                'gravatarUrl' => 'https://gravatar.com/avatar/' . md5(strtolower($fields['email'] ?? '')),
+                'fieldNames' => array_keys($fields),
+                'fields' => $fields,
             ],
             new ComponentHashmap([
             ])
